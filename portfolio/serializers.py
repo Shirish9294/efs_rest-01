8257861728716436ce213e397291c75f586cd3f8
Shirish9294 <37794995+Shirish9294@users.noreply.github.com>
2021-08-05 08:12:18 -0500

    update-1

---
+++ portfolio/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Customer, Investment, Stock
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+            model = Customer
+            fields = ('pk','name', 'address', 'city', 'state', 'zipcode', 'email', 'email', 'cell_phone')
+
+class InvestmentSerializer(serializers.ModelSerializer):
+    cust_name = serializers.CharField(read_only=True, source="customer.name")
+
+    class Meta:
+            model = Investment
+            fields = ('pk','customer','cust_number', 'cust_name', 'category', 'description', 'acquired_value', 'acquired_date', 'recent_value', 'recent_date')
+
+
+class StockSerializer(serializers.ModelSerializer):
+    cust_name = serializers.CharField(read_only=True, source="customer.name")
+
+    class Meta:
+            model = Stock
+            fields = ('pk','customer', 'cust_number', 'cust_name', 'symbol', 'name', 'shares', 'purchase_price', 'purchase_date')
